3f730fda63dabd492f4673f5266112686b6c5a09
Adrian Conlin <aconlin@scottlogic.com>
2018-11-21 11:59:01 +0000

    Added app title.

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Fibonacci Calculator</h1>
             <Link to="/">Home</Link>
             <Link to="/admin">Admin</Link>
           </header>
